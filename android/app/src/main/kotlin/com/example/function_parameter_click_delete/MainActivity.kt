c85db4362c66ccf2073754d8313f229af648c626
KienNguyen <83253750+KienNguyen2021@users.noreply.github.com>
2024-11-10 21:22:30 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/function_parameter_click_delete/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.function_parameter_click_delete
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/quote.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+
+class Quote {
+
+  String text;
+  String author;
+
+  //  normal constructor, as we've already seen
+
+  //  Quote(String author, String text){
+  //    this.text = text;
+  //    this.author = author;
+  //  }
+
+  //  constructor with named parameters
+
+  //  Quote({ String author, String text }){
+  //    this.text = text;
+  //    this.author = author;
+  //  }
+
+  // constructor with named parameters
+  // & automatically assigns named arguments to class properties
+
+  Quote({ this.text, this.author });
+
+}
